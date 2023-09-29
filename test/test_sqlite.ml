05fa6b82b5f53f9cc59c10916e0f4e0caa73a6b9
David Engelmann <david+4113@satisfilabs.com>
2023-09-29 15:23:12 -0400

    add a testing suite for sqlite

---
+++ test/test_sqlite.ml
@@ -0,0 +1,34 @@
+open Alcotest
+open Libsql.Sqlite
+
+let sample_basic_sqlite_config : Sqlite.config =
+  { scheme = "file"; authority = "localhost"; path = "local.db" }
+
+let test_sample_basic_sqlite_config_scheme () =
+  match sample_basic_sqlite_config with
+  | { scheme; _ } -> check string "same scheme" "file" scheme
+
+let test_sample_basic_sqlite_config_authority () =
+  match sample_basic_sqlite_config with
+  | { authority; _ } -> check string "same authority" "localhost" authority
+
+let test_sample_basic_sqlite_config_path () =
+  match sample_basic_sqlite_config with
+  | { path; _ } -> check string "same path" "local.db" path
+
+let () =
+  Alcotest.run "Sqlite Test Suite"
+    [
+      ( "Sqlite",
+        [
+          ( "test_sample_basic_sqlite_config_scheme",
+            `Quick,
+            test_sample_basic_sqlite_config_scheme );
+          ( "test_sample_basic_sqlite_config_authority",
+            `Quick,
+            test_sample_basic_sqlite_config_authority );
+          ( "test_sample_basic_sqlite_config_path",
+            `Quick,
+            test_sample_basic_sqlite_config_path );
+        ] );
+    ]
